a3f7a31df899c379801b0be1b0811c2a8957ef0d
shurushku <nastia.kuchina.741456963@gmail.com>
2022-11-28 17:17:04 +0200

    Change load_dotenv in settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -18,6 +18,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -83,8 +84,6 @@ WSGI_APPLICATION = "cinema_service.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-load_dotenv()
 
 DATABASES = {
     "default": {
